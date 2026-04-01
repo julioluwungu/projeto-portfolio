2901b405984ccc363d30603500b8707e0174f970
Júlio Luwungu <julioluwungu@gmail.com>
2026-04-01 19:41:19 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,11 @@ botao.addEventListener('click', () => {
   const isescuro = body.classList.toggle('escuro');
   temaEscuro(isescuro);
   localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
-  foto.src = foto.src.includes('claro') ? foto.src.replace('claro', 'escuro') : foto.src.replace('escuro', 'claro');
+  if (localStorage.getItem('tema') == 'escuro' && foto.src.includes('perfil-claro.png')) {
+    foto.src = foto.src.replace('claro', 'escuro');
+  } else {
+    foto.src = foto.src.replace('escuro', 'claro');
+  }
 });
 
 // Scroll suave para links de navegação
